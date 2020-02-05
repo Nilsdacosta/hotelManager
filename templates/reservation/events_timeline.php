995ac1db2efac94dcb706fb1d123cd3118bd8e97
TeamKeepers <audrey.thirard@hotmail.fr>
2020-02-05 13:14:10 +0100

    fullcalendar

---
+++ templates/reservation/events_timeline.php
@@ -0,0 +1,65 @@
+<?php
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+    require_once("inc/init.php");
+
+    
+class Reservation extends ServiceEntityRepository {
+    
+
+    public $start;
+    public $end;
+    public $rendering;
+    public $backgroundColor;
+    public $idarticle;
+    public $idbooked;
+
+
+}
+
+
+      ################################################################
+      ################## AFFICHAGE FULLCALENDAR ######################
+      ################################################################
+
+
+
+      # je crée la requete pour récupérer les info de la table booked
+      $req_findBooked = "SELECT * FROM reservation ";
+      $pdoST_find_booked = $pdo->prepare($req_findBooked);
+
+      if($pdoST_find_booked->execute()){
+          //debug(json_encode($pdoST_find_booked->fetchAll())) ;
+          //json_encode($pdoST_find_booked->fetchAll());
+
+          //debug(json_decode($json));
+
+          $findbookeds=$pdoST_find_booked->fetchAll();
+          $tabEvents = array();
+          sleep(1);
+           foreach ($findbookeds as $findbooked) {
+               //debug($findbookeds);
+               $event = new Reservation();
+               $event->start=$findbooked['date_arrival'];
+               $event->end=$findbooked['date_departure'];
+               $event->rendering='background';
+               $event->backgroundColor='#FE1919';
+               $event->idarticle=$findbooked['fk_id_article'];
+               $event->idbooked=$findbooked['id_booked'];
+              
+               array_push($tabEvents, $event);
+           }
+           echo json_encode($tabEvents);
+      }
+
+
+
+
+    # je gère de délais de chargement de mes infos
+    
+   
+
+
+
+?>
