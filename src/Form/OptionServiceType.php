12041c0191a35d613556a9b762b102c488a751e0
Virginie88 <59996545+Virginie88@users.noreply.github.com>
2020-02-06 11:26:03 +0100

    maj login mdp option service

--- src/Form/OptionServiceType.php
+++ src/Form/OptionServiceType.php
@@ -2,9 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\Tva;
 use App\Entity\OptionService;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
@@ -14,18 +16,14 @@ class OptionServiceType extends AbstractType
     {
         $builder
             ->add('nomOption')
-            ->add('dateCreation')
+            //->add('dateCreation')
             ->add('prixOption')
             //->add('employe')
-            // //->add('tva', ChoiceType::class, [
-            //     'choices'  => [
-            //         '20' => 20,
-            //         '5' => 5,
-                   
-            //     ],
-            //     'expanded' => true
-            // ])
-            // ->add('reservations')
+            ->add('tva', EntityType::class, [
+                'class'=> Tva::class ,
+                'choice_label' => ' nom',
+                  'expanded' => true,
+             ])
         ;
     }
 
