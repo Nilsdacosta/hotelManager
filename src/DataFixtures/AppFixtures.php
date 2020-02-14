95541ef9b720f2f4fb3c51e708f5816f651dfa6f
Nilsdacosta <nilsdacosta@gmail.com>
2020-02-14 21:03:28 +0100

    Contournement protocole secu

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Employe;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $employe = new Employe();
+
+        $employe->setNom('Da Costa')
+            ->setPrenom('Nils')
+            ->setUsername('dacostanils123')
+            ->setPassword('dacostanils123')
+            ->setPoste('directeur')
+            ->setTelephone('0606060606');
+        $manager->persist($employe);
+
+        $manager->flush();
+    }
+}
